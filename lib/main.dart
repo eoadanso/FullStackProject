61ebeeba8d20075073b8ae25edf06e0489c8760a
eoadanso <aericdanso98@gmail.com>
2022-10-26 16:25:14 +0000

    worked on firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:proper_project/Util/colors.dart';
 import 'package:proper_project/responsive/mobile_screen_layout.dart';
 import 'package:proper_project/responsive/responsive_layout_screen.dart';
 import 'package:proper_project/responsive/web_screen_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp() ;
   runApp(const MyApp());
 }
 
