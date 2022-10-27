6961f6779d424efe7bd2559345ab4aec7c702c9f
eoadanso <aericdanso98@gmail.com>
2022-10-27 12:28:15 +0000

    Added a login UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:proper_project/Util/colors.dart';
 import 'package:proper_project/responsive/mobile_screen_layout.dart';
 import 'package:proper_project/responsive/responsive_layout_screen.dart';
 import 'package:proper_project/responsive/web_screen_layout.dart';
+import 'package:proper_project/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp() ;
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBMg-CZUfDuUs3OOyPtRaXFj6i5mXm1pLI",
+          appId: "1:568989070936:web:9c3766f3f478cc92487c2d",
+          messagingSenderId: "568989070936",
+          projectId: "fullstackproject-7013b",
+          storageBucket: "fullstackproject-7013b.appspot.com",
+      )
+    ) ;
+  }
+  else{
+    await Firebase.initializeApp() ;
+  }
   runApp(const MyApp());
 }
 
@@ -23,7 +38,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor
       ),
-      home: const ResponsiveLayout(mobileScreenLayout: MobileScreenLayout() , webScreenLayout: WebScreenLayout(),)
+      // home: const ResponsiveLayout(mobileScreenLayout: MobileScreenLayout() , webScreenLayout: WebScreenLayout(),)
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:proper_project/util/colors.dart';
+import 'package:proper_project/widgit/text_field_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    // Clear off the controllers as soon the widgets get disposed
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              // svg image
+              SvgPicture.asset(
+                "assets/ic_instagram.svg",
+                color: primaryColor,
+                height: 64,
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              // text field input for email
+              TextInputField(
+                hintText: "Enter your email",
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailController,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              // text field input for password
+              TextInputField(
+                hintText: "Enter your password",
+                textInputType: TextInputType.text,
+                textEditingController: _passwordController,
+                isPass: true,
+              ),
+              // button for logging in
+              const SizedBox(
+                height: 25,
+              ),
+              InkWell(
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4)))),
+                  child: const Text("Log in"),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              // transition for sign up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: const Text("Don't have an account ? "),
+                  ),
+                  const SizedBox(
+                    width: 2,
+                  ),
+                  GestureDetector(
+                    onTap: (){},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: const Text(
+                        "Sign up ",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
